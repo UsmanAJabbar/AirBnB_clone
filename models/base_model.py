89a03eece59418f90c4795fd5710893ec1781f2e
usmangta <usmangta@gmail.com>
2020-06-25 23:42:47 +0000

    added fiddly code for debug

--- models/base_model.py
+++ models/base_model.py
@@ -2,6 +2,7 @@
 """BaseModel"""
 from datetime import datetime
 import uuid
+from models import storage
 
 
 class BaseModel():
@@ -18,12 +19,21 @@ class BaseModel():
     #           MAGIC METHODS         #
     # ------------------------------- #
     def __init__(self, *args, **kwargs):
-        ""
-        ""
+        """
+        ----------------------
+        MAGIC METHOD: __INIT__
+        ----------------------
+        DESCRIPTION:
+            Initializes the needed attributes
+        Args:
+            @args: a number of arguments
+            @kwargs: a dictionary of attributes
+        """
         if not kwargs or len(kwargs) == 0:
             self.id = str(uuid.uuid4())
             self.created_at = datetime.now()
             self.updated_at = datetime.now()
+            storage.new(self)
         else:
             for key in kwargs.keys():
                 if key != '__class__':
@@ -68,6 +78,7 @@ class BaseModel():
             @self: current instance
         """
         self.updated_at = datetime.now()
+        storage.save()
 
     def to_dict(self):
         """
---
+++ models/engine/file_storage.py
@@ -0,0 +1,71 @@
+#!/usr/bin/python3
+"""CLASS FILE STORAGE"""
+import json
+from models.base_model import BaseModel
+from os.path import exists
+
+class FileStorage(BaseModel):
+    """
+    ------------------
+    CLASS: FileStorage
+    ------------------
+    """
+
+    __file_path = "test.json"
+    __objects = {}
+
+    def all(self):
+        """
+        ---------------------------
+        PUBLIC INSTANCE METHOD: ALL
+        ---------------------------
+        DESCRIPTION:
+            Returns the dictionary stored in
+            the attribute '__objects'
+        ARGS:
+            @self: current instance
+        """
+        return self.__objects
+
+    def new(self, obj):
+        """
+        ---------------------------
+        PUBLIC INSTANCE METHOD: NEW
+        ---------------------------
+        DESCRIPTION:
+            Adds the necessary objects to the
+            '__objects' attribute
+        ARGS:
+            @self: current instance
+            @obj: object to add to '__objects'
+        """
+        self.__objects[obj.__class__.__name__ + '.' + obj.id] = obj
+
+    def save(self):
+        """
+        ----------------------------
+        PUBLIC INSTANCE METHOD: SAVE
+        ----------------------------
+        DESCRIPTION:
+            Serializes items in __objects to JSON
+            and dumps the output into a file defined
+            by '__file_path'
+        ARGS:
+            @self: current instance
+        """
+        temp = {}
+
+        for keys in self.__objects.keys():
+            temp[keys] = self.__objects[keys].to_dict()
+
+        with open(self.__file_path, mode='w') as jsonfile:
+            json.dump(temp, jsonfile)
+
+    def reload(self):
+        """
+        """
+        if exists(self.__file_path):
+            with open(self.__file_path) as jsonfile:
+                deserialized = json.load(jsonfile)
+            for keys in deserialized.keys():
+                self.__objects[keys] = BaseModel(**deserialized[keys])
